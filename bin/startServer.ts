2591be95eb6897e68b01515e154f3091d4ef72a8
Mohammad Ahmad <mohammad.ahmad@carbonteq.com>
2024-06-26 15:30:25 +0500

    minor fixes and added cors

--- bin/startServer.ts
+++ bin/startServer.ts
@@ -1,12 +1,13 @@
 import express from "express";
 import {petsRouter} from "../http/routes/petRoutes";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
-
+app.use(cors());
 app.use("/petsapp/pet", petsRouter);
 
-app.listen(Number(3020), "0.0.0.0", () => {
+app.listen(Number(3020), () => {
     console.log(`DEV Server running on port ${3020}`);
 });
