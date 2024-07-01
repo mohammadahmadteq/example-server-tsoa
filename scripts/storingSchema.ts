161b7e3443d2726f45c45cd991df7198eca069ea
Ali Saif <muhammadalisaif2@gmail.com>
2024-07-01 18:37:46 +0500

    Preventing Multiple Copies of Schema

--- scripts/storingSchema.ts
+++ scripts/storingSchema.ts
@@ -1,20 +1,37 @@
 import { google } from 'googleapis';
 import fs from 'fs';
-// import dotenv from 'dotenv';
+import dotenv from 'dotenv';
 
-// dotenv.config();
+dotenv.config();
 
 async function uploadFile() {
   const auth = new google.auth.GoogleAuth({
     keyFile: './credentials.json',
-    scopes: ['https://www.googleapis.com/auth/drive.file'],
+    scopes: ['https://www.googleapis.com/auth/drive'],
   });
 
   const drive = google.drive({ version: 'v3', auth });
 
+  try {
+    const listResponse = await drive.files.list({
+      q: "name='swagger.json' and trashed=false",
+      fields: 'files(id, name)',
+    });
+
+    const files = listResponse.data.files;
+    if (files.length > 0) {
+      for (const file of files) {
+        await drive.files.delete({ fileId: file.id });
+      }
+    }
+  } catch (error) {
+    console.error('Error searching/deleting files:', error);
+    return;
+  }
+
   const fileMetadata = {
     name: 'swagger.json',
-    parents: ['1cRBjK_sBYiy1DtHhjC0nHXoG7tHol9zO'],
+    parents: [process.env.GOOGLE_DRIVE_FOLDER_ID],
   };
   const media = {
     mimeType: 'application/json',
