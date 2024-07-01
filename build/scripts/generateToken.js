e0b4504d226f7006ce6f712fd7ffad1acb4850e8
root <root@DESKTOP-FUUHGOR.localdomain>
2024-07-01 23:47:05 +0500

    Uploaded uploadSwagger

--- build/scripts/generateToken.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const authentication_1 = require("../src/authentication");
-const payload = {
-    id: 1,
-    name: 'John Doe'
-};
-const token = (0, authentication_1.generateToken)(payload);
-console.log(`Generated Token: ${token}`);
